360aa48522558ed80e650316029fdcc659183fc7
Abhiabhishek5124 <78491711+Abhiabhishek5124@users.noreply.github.com>
2024-08-05 00:02:39 -0400

    initial table design using material ui

---
+++ pantry/app/page.js
@@ -0,0 +1,45 @@
+import { Box, Stack, Typography } from "@mui/material";
+
+const item= ['tomato', 'potato', 'onion', 'garlic', 'carrot', 'tomato', 'potato', 'onion', 'garlic', 'carrot', 'tomato', 'potato', 'onion', 'garlic', 'carrot']
+
+export default function Home() {
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display={'flex'} 
+      justifyContent={'center'} 
+      flexDirection={'column'}
+      alignItems={'center'}>
+    
+      <Box width="800px" height="100px" bgcolor={'#ADD8E6'}>
+        <Typography variant="h2" color={'#333'} textAlign={'center'} >
+          Pantry Items
+        </Typography>
+      </Box>
+    
+      <Stack width="800px" height="300px" spacing={2} overflow={'auto'} border={'1px solid #333'} >
+        {item.map((i) => (
+          <Box key={i} 
+            width="100%" height="100px" 
+            display={'flex'} 
+            justifyContent={'center'} 
+            alignItems={'center'} 
+            bgcolor={'#f0f0f0'}
+          >
+            <Typography 
+              variant="h4" 
+              color={'#333'} 
+              textAlign={'center'} 
+              fontWeight={'bold'}
+            >
+              {
+                i.charAt(0).toUpperCase() + i.slice(1)
+              }
+            </Typography>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
+  );
+}
